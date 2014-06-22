cf8796bbd9c268d887690de1259d9a2ee775897e
CandyLin <ppyuan@gmail.com>
2014-06-22 23:06:11 +0800

    Update and rename cachematrix.R to makeCacheMatrix.R

---
+++ makeCacheMatrix.R
@@ -0,0 +1,43 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+}
+
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        m <-x$getsolve()
+        if(!is.null(m)){
+                message("getting inverse data")
+                return(m)
+        }
+        data <-x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+        
+        ## same to return diag(dim(x)[1])/x
+}
+
+
+
+        
+        
+        
+        
+        
